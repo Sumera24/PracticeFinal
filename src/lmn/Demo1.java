43603772ed94905e51d6f7869e1a02e808e09fc9
Sumera24 <sumeraqz@gmail.com>
2020-12-07 13:49:15 -0500

    Commit from Mr.X

---
+++ src/lmn/Demo1.java
@@ -0,0 +1,12 @@
+package lmn;
+
+public class Demo1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("First print by Mr.X");
+		
+		
+	}
+
+}
